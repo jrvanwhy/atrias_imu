addcb092f8129571c338df87142e8121bc517504
Johnathan Van Why <jrvanwhy@gmail.com>
2015-01-09 15:45:55 -0800

    Moving things over to atrias-matlab

--- IMUSysState.m
+++
@@ -1,8 +0,0 @@
-classdef IMUSysState < uint8
-	enumeration
-		INIT(1)  % Waiting for first data
-		ALIGN(2) % Conducting the alignment process
-		RUN(3)   % Running (producing accurate output)
-		FAIL_ALIGN(4) % The alignment has failed!
-	end
-end
