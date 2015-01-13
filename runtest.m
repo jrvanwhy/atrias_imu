8bb7c892e3adb685479381a0c50097c193f405ad
Johnathan Van Why <jrvanwhy@gmail.com>
2015-01-13 00:25:49 -0800

    Working on a more advanced alignment procedure

--- runtest.m
+++ runtest.m
@@ -4,16 +4,25 @@ close all
 imu = IMUSys;
 bc  = BoomCoords;
 
-load('imu_test_walk')
+latitude = 44.5673031; % The DRL's latitude, according to Google Maps
+
+load('~/imu_test_walk')
+
+startTime = 2.023e6 - 1 * 60 * 1000;
+state = state(startTime:end, :);
+time  = time(startTime:end);
+len = 2 * 60 * 1000;
+state = state(1:len, :);
+time  = time(1:len);
+
 A = AtriasPostProcess(state, time);
 
 % Figure out when our data cuts out
-len = 1 + find(diff(A.controllerData(:, 7)), 1, 'last');
+%len = 1 + find(diff(A.controllerData(:, 7)), 1, 'last');
+%len = numel(time);
 
 % Pre-allocate things
 imu_states   = zeros(len, 1  );
-align_accums = zeros(3,   len);
-accum_norms  = zeros(len, 1  );
 test_out     = zeros(3,   len);
 rolls        = zeros(len, 1  );
 pitches      = zeros(len, 1  );
@@ -29,8 +38,6 @@ for iter = 1:len
 	end
 	[imu_orient,local_orient,ang_vel,state2] = imu.update(A.controllerData(iter, 1:3), A.controllerData(iter, 4:6), A.controllerData(iter,7), .001);
 	imu_states(iter) = imu.state;
-	align_accums(:, iter) = imu.align_accum;
-	accum_norms(iter)     = norm(imu.align_accum);
 	[rolls(iter),pitches(iter),yaws(iter),drolls(iter),dpitches(iter),dyaws(iter)] = ...
 		bc.update(imu_orient, local_orient, ang_vel, state2, A.boomRollAngle(iter), A.boomPitchAngle(iter), A.boomYawAngle(iter));
 end
